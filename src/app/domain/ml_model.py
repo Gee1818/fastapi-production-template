8a6b29b5f86252b25cbc465eabe21b0a76562a7b
Gee1818 <g1sw@hotmail.com>
2025-09-27 08:06:12 -0300

    chore(): make ml_model fit type morpe specific

--- src/app/domain/ml_model.py
+++ src/app/domain/ml_model.py
@@ -1,7 +1,9 @@
-from typing import Any, Protocol, runtime_checkable
+from typing import Any, Protocol, Self, runtime_checkable
+
+from polars import DataFrame, Series
 
 
 @runtime_checkable
 class MLModel(Protocol):
     def predict(self, X: Any, **kwargs: Any) -> Any: ...
-    def fit(self, X: Any, y: Any, **kwargs: Any) -> Any: ...
+    def fit(self, X: DataFrame, y: Series) -> Self: ...
