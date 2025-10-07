a39cba12532e51dab0c1372d6630f8081d63a05d
Gee1818 <g1sw@hotmail.com>
2025-10-06 22:50:40 -0300

    chore(): correct mark ups from review

--- src/app/domain/ml_model.py
+++ src/app/domain/ml_model.py
@@ -1,9 +1,13 @@
-from typing import Any, Protocol, Self, runtime_checkable
+from collections.abc import Sequence
+from typing import Protocol, Self, runtime_checkable
 
+from numpy import ndarray
 from polars import DataFrame, Series
 
 
 @runtime_checkable
 class MLModel(Protocol):
-    def predict(self, X: Any, **kwargs: Any) -> Any: ...
+    def predict(
+        self, X: Sequence[Sequence[float]]
+    ) -> ndarray | tuple[ndarray, ndarray]: ...
     def fit(self, X: DataFrame, y: Series) -> Self: ...
