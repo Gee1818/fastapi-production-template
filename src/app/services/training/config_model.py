e4f9fd789a491ad9d4c2cf87296841f619b17901
Gee1818 <g1sw@hotmail.com>
2025-11-23 22:53:52 -0300

    feature(train): fix model parameters type

--- src/app/services/training/config_model.py
+++ src/app/services/training/config_model.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Literal
 
 
 @dataclass
@@ -7,7 +8,7 @@ class ModelConfig:
     n_estimators: int = 100
     max_depth: int = 10
     min_samples_leaf: int = 2
-    max_features: str = "sqrt"
+    max_features: Literal["sqrt", "log2"] | float = "sqrt"
     min_samples_split: int = 80
     max_samples: float = 0.5
     random_state: int = 42
